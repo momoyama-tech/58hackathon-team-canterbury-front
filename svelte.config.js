1d215934a0802263ec64e446b079d4d30900ac1b
Hideyasu <kutuzov1228@gmail.com>
2025-02-14 09:30:54 +0900

    feat: netlifyにデプロイ先を変更

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 const production = process.env.NODE_ENV === 'production';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			fallback: 'index.html' // すべてのリクエストを index.html にフォールバック
-		}),
-		paths: {
-            base: production ? '/58hackathon-team-canterbury-front' : '',
-        }
+		adapter: adapter(),
 	}
 };
 
