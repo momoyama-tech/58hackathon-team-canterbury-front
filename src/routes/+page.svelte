ef3250e9f7b8cb67303f04c0352a73c15029f843
Hideyasu <kutuzov1228@gmail.com>
2025-02-16 02:00:49 +0900

    feat: topのページにモーダルを追加

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,71 @@
 <script>
     import "./style.css";
+
+    let isProfileModalOpen = true;
+    let isRoomModalOpen = false;
+
+    function openProfileModalOpen() {
+        isProfileModalOpen = true;
+    }
+
+    function closeProfileModalOpen() {
+        isProfileModalOpen = false;
+    }
+
+    function openRoomModalOpen() {
+        isRoomModalOpen = true;
+    }
+
+    function closeRoomModalOpen() {
+        isRoomModalOpen = false;
+    }
 </script>
 
-<h1>WeReal?</h1>
-<button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
-<button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Dark</button>
+<h1 class="text-3xl font-bold mb-4 text-white">WeReal?</h1>
+<button class="px-4 py-2 bg-blue-500 text-white rounded">
+    部屋を作成
+</button>
+<button on:click={openRoomModalOpen} class="px-4 py-2 bg-blue-500 text-white rounded">
+    部屋に入室
+</button>
+
+{#if isProfileModalOpen}
+  <div class="fixed top-0 left-0 h-screen bg-black bg-opacity-50">
+    <div class="h-screen bg-gray-900 text-white p-6 rounded-b-lg w-full max-w-md shadow-lg relative">
+      <button on:click={closeProfileModalOpen} class="absolute top-4 left-4 text-2xl">×</button>
+      <h2 class="text-2xl font-bold mb-4 text-center">プレイヤー設定</h2>
+      <div class="space-y-4">
+        <div>
+          <label class="block mb-1">アバター:</label>
+          <input type="file" class="border-2 border-gray-300 rounded-lg w-full p-2">
+        </div>
+        <div>
+          <label class="block mb-1">ニックネーム:</label>
+          <input type="text" class="border-2 border-gray-300 rounded-lg w-full p-2">
+        </div>
+      </div>
+    </div>
+  </div>
+{/if}
+
+{#if isRoomModalOpen}
+  <div class="fixed top-0 left-0 h-screen bg-black bg-opacity-50">
+    <div class="h-screen w-screen bg-gray-900 text-white p-6 rounded-b-lg w-full max-w-md shadow-lg relative">
+      <button on:click={closeRoomModalOpen} class="absolute top-4 left-4 text-2xl">×</button>
+      <h2 class="text-2xl font-bold mb-4 text-center">Room番号</h2>
+      <div class="space-y-4">
+        <div>
+          <input type="text" class="border-2 border-gray-300 rounded-lg w-full p-2">
+        </div>
+        <button class="px-4 py-2 bg-blue-500 text-white rounded">
+            部屋に入室
+        </button>
+        <div>
+            <button class="w-full py-2 bg-gray-700 hover:bg-gray-600 text-white rounded flex items-center justify-center">
+                📷 カメラ
+            </button>
+        </div>
+      </div>
+    </div>
+  </div>
+{/if}
