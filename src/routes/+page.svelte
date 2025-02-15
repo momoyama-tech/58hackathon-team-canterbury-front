ced7384a8ecec153fffec543852c844452528d37
Asuma09 <241l002@andrew.ac.jp>
2025-02-15 22:41:03 +0900

    feat:add iconComponent

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
+
+<script>
+    import BasicIcon from "../components/Icons/BasicIcon/index.svelte"
+</script>
+    <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<BasicIcon src="/images/defaultIcon.png" size=50/>
