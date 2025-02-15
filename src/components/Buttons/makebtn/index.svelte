a91ade94fe07b6c8eee1bc01874e3f93d873c428
nadir <m11062@nara.kosen-ac.jp>
2025-02-16 02:47:56 +0900

    feat-top01

--- src/components/Buttons/makebtn/index.svelte
+++ src/components/Buttons/makebtn/index.svelte
@@ -1,11 +1,10 @@
 <script>
     import "./style.css"
-    export let label = 'Click Me'; // ボタンのラベル
+    export let label = '部屋を作成'; // ボタンのラベル
     export let onClick = () => {}; // クリック時の処理
     
   </script>
   
-  <button on:click={onClick} class="join-btn">
+  <button on:click={onClick} class="mk-btn">
     {label}
   </button>
-  
