ca6e0a9ec68aa885d0a3fb0c41412d0cfe5a9393
nadir <m11062@nara.kosen-ac.jp>
2025-02-16 07:29:23 +0900

    fix:ボタンのコンポーネント名の変更

--- src/routes/design/01/+page.svelte
+++ src/routes/design/01/+page.svelte
@@ -2,14 +2,14 @@
 <script>
     import Logo from '$src/components/Logos/Logo/index.svelte';
     import "./style.css";
-    import Btn from '$src/components/Buttons/btn/index.svelte';
+    import BasicButton from '$src/components/Buttons/btn/index.svelte';
 </script>
 
 <main class="main">
     <Logo/>
     <div class="btn">
-       <Btn label="部屋を作成"/>
-       <Btn label="部屋に入室"/>
+       <BasicButton label="部屋を作成"/>
+       <BasicButton label="部屋に入室"/>
     </div>
 
     
