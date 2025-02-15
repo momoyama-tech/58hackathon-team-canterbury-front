d2d66657baca77f52d2c1f4f22025da34184f2b9
nadir <m11062@nara.kosen-ac.jp>
2025-02-16 04:57:06 +0900

    fix-button

--- src/routes/design/01/+page.svelte
+++ src/routes/design/01/+page.svelte
@@ -2,15 +2,14 @@
 <script>
     import Logo from '$src/components/Logos/Logo/index.svelte';
     import "./style.css";
-    import Makebtn from '$src/components/Buttons/makebtn/index.svelte';
-    import Joinbtn from '$src/components/Buttons/joinbtn/index.svelte';
+    import Btn from '$src/components/Buttons/btn/index.svelte';
 </script>
 
 <main class="main">
     <Logo/>
     <div class="btn">
-       <Makebtn/>
-       <Joinbtn/>
+       <Btn label="部屋を作成"/>
+       <Btn label="部屋に入室"/>
     </div>
 
     
