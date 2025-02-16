db9e6ca1376cda20a4945731adeae6040c215b47
Yuuta Sakurai <sakurai@andrew.ac.jp>
2025-02-16 12:04:57 +0900

    vote画面デザイン仮修正(チェックボックス右寄せ)

--- src/features/volt/components/VoteCard/index.svelte
+++ src/features/volt/components/VoteCard/index.svelte
@@ -1,55 +1,55 @@
 <script>
+	export let username;
+	export let imageUrl;
+	export let checkedTop = false;
+	export let checkedBottom = false;
 
-    export let username;
-    export let imageUrl;
-    export let checkedTop = false;
-    export let checkedBottom = false;
+	let isCheckedTop = checkedTop;
+	let isCheckedBottom = checkedBottom;
 
-    let isCheckedTop = checkedTop;
-    let isCheckedBottom = checkedBottom;
+	function toggleCheck_Top() {
+		isCheckedTop = !isCheckedTop;
+	}
 
-    function toggleCheck_Top() {
-        isCheckedTop = !isCheckedTop;
-    }
-
-    function toggleCheck_Bottom() {
-        isCheckedBottom = !isCheckedBottom;
-    }
+	function toggleCheck_Bottom() {
+		isCheckedBottom = !isCheckedBottom;
+	}
 </script>
 
-<label class="flex items-center cursor-pointer label-checkbox">
-<div class="flex items-center p-4 bg-gray-800 rounded-lg shadow-lg text-white full-width">
-    <!-- チェックボックス -->
-        <div class="username">
-            <span class="username flex flex-column text-lg font-semibold">{username}</span>
+<label class="">
+	<div class="full-width">
+		<!-- チェックボックス -->
+		<div class="username">
+			<span>{username}</span>
+		</div>
 
-        </div>
+		<div class="flex-end">
+			<input
+				type="checkbox"
+				checked={isCheckedTop}
+				class="vote-checkbox hidden"
+				on:change={toggleCheck_Top}
+			/>
+		</div>
 
-        <div class="flex-end">
-
-            <input type="checkbox" checked={isCheckedTop} class="hidden vote-checkbox" on:change={toggleCheck_Top} />
-
-        </div>
-
-        <div class="w-6 h-6 flex justify-center items-center border-2 border-gray-400 rounded-full mr-3">
-            {#if isCheckedTop}
-                <span class="text-purple-400 text-xl font-bold"></span>
-            {/if}
-        </div>
-</div>
+		<div class="">
+			{#if isCheckedTop}
+				<span class="text-xl font-bold text-purple-400"></span>
+			{/if}
+		</div>
+	</div>
 </label>
 
 <style>
-    .full-width {
-        width: 100%;
-        display: flex;
-    }
-.vote-checkbox {
-    flex: 1;
-    justify-content: flex-end;
-}
-.username {
-    flex: 3;
-
-}
+	.full-width {
+		width: 100%;
+		display: flex;
+	}
+	.username {
+		flex: 1;
+	}
+	.vote-checkbox {
+		flex: 1;
+		margin-left: auto;
+	}
 </style>
--- src/routes/design/04/+page.svelte
+++ src/routes/design/04/+page.svelte
@@ -1,19 +1,16 @@
 <script>
-  import "./style.css";
-  import VoteSection from "$src/features/volt/components/VoteSection/index.svelte"
-  import VoteCard from "$src/features/volt/components/VoteCard/index.svelte"
+	import './style.css';
+	import VoteSection from '$src/features/volt/components/VoteSection/index.svelte';
+	import VoteCard from '$src/features/volt/components/VoteCard/index.svelte';
 
-  let candidate = {
-    username: "ユーザーネーム",
-    imageUrl: "https://via.placeholder.com/300x200",
-    checked: true
-  };
+	let candidate = {
+		username: 'ユーザーネーム',
+		imageUrl: 'https://via.placeholder.com/300x200',
+		checked: true
+	};
 </script>
 
- <div class="voltContainer">
-  <!-- タイマー -->
-  <VoteSection/>
-  <!-- 投票カード -->
-  <VoteCard {...candidate} />
-  
-</div>
+<!-- タイマー -->
+<VoteSection />
+<!-- 投票カード -->
+<VoteCard {...candidate} />
