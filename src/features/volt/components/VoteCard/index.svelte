fd40d33fa436a3123ba02c15ea97cfd0398e6039
usagi <s11376@nara.kosen-ac.jp>
2025-02-16 11:34:17 +0900

    feat Volt

--- src/features/volt/components/VoteCard/index.svelte
+++ src/features/volt/components/VoteCard/index.svelte
@@ -2,30 +2,54 @@
 
     export let username;
     export let imageUrl;
-    export let checked = false;
+    export let checkedTop = false;
+    export let checkedBottom = false;
 
-    let isChecked = checked;
+    let isCheckedTop = checkedTop;
+    let isCheckedBottom = checkedBottom;
 
-    function toggleCheck() {
-        isChecked = !isChecked;
+    function toggleCheck_Top() {
+        isCheckedTop = !isCheckedTop;
+    }
+
+    function toggleCheck_Bottom() {
+        isCheckedBottom = !isCheckedBottom;
     }
 </script>
 
-
-<div class="flex items-center p-4 bg-gray-800 rounded-lg shadow-lg text-white">
+<label class="flex items-center cursor-pointer label-checkbox">
+<div class="flex items-center p-4 bg-gray-800 rounded-lg shadow-lg text-white full-width">
     <!-- チェックボックス -->
-    <label class="flex items-center cursor-pointer">
-        <input type="checkbox" bind:checked={isChecked} class="hidden" on:change={toggleCheck} />
+        <div class="username">
+            <span class="username flex flex-column text-lg font-semibold">{username}</span>
+
+        </div>
+
+        <div class="flex-end">
+
+            <input type="checkbox" checked={isCheckedTop} class="hidden vote-checkbox" on:change={toggleCheck_Top} />
+
+        </div>
+
         <div class="w-6 h-6 flex justify-center items-center border-2 border-gray-400 rounded-full mr-3">
-            {#if isChecked}
-                <span class="text-purple-400 text-xl font-bold">✔</span>
+            {#if isCheckedTop}
+                <span class="text-purple-400 text-xl font-bold"></span>
             {/if}
         </div>
-    </label>
-
-    <!-- ユーザー名と画像 -->
-    <div class="flex flex-col">
-        <span class="text-lg font-semibold">{username}</span>
-        <img src="{imageUrl}" alt="{username}" class="mt-2 w-64 h-40 object-cover rounded-lg border border-gray-500">
-    </div>
 </div>
+</label>
+
+<style>
+    .full-width {
+        width: 100%;
+        display: flex;
+    }
+.vote-checkbox {
+    flex: 1;
+    justify-content: flex-end;
+}
+.username {
+    flex: 3;
+
+}
+</style>
